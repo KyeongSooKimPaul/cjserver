80bd7f9fd244b5ddcc16a96547e9c94d955535cc
kyeongsoo kim <with317@gmail.com>
2022-06-18 21:56:34 +0900

    df

---
+++ src/permissions/index.ts
@@ -0,0 +1,37 @@
+import { allow, rule, shield } from 'graphql-shield'
+import { getUserId } from '../utils'
+
+const rules = {
+  isAuthenticatedUser: rule()((parent, args, context) => {
+    const userId = getUserId(context)
+    return Boolean(userId)
+  }),
+  isPostOwner: rule()(async (parent, { id }, context) => {
+    const userId = getUserId(context)
+    const author = await context.prisma.tweet
+      .findOne({
+        where: {
+          id: Number(id),
+        },
+      })
+      .author()
+    return userId === author.id
+  }),
+}
+
+// export const permissions = shield({})
+
+export const permissions = shield({
+  Query: {
+    // me: rules.isAuthenticatedUser,
+    // filterPosts: rules.isAuthenticatedUser,
+    // tweet: rules.isAuthenticatedUser,
+    "*": allow,
+  },
+  Mutation: {
+    // createDraft: rules.isAuthenticatedUser,
+    // deletePost: rules.isPostOwner,
+    // publish: rules.isPostOwner,
+    "*": allow,
+  },
+})
